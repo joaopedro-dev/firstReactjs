84f60514dc54e0c5d6b8e8e77080f27c5b3c5ddb
Joao Pedro Barros <joaobarros11@hotmail.com>
2019-06-13 22:16:00 -0300

    First hands on with React Js.

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.css';
+
+const Header = () => (
+    <header id="main-header">JShunt</header>
+);
+
+export default Header;
+// class Header extends Component {
+//     render(){
+//         return <h1>Hello</h1>
+//     }
+// }
---
+++ src/pages/main/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import api from "../../services/api";
+
+export default class Main extends Component {
+    render(){
+        return <h1>Hello React</h1>;
+    }
+}
