63425585e05103178af4cf281020edae433ab9d4
Joao Pedro Barros <joaobarros11@hotmail.com>
2019-06-14 19:20:08 -0300

    upgranding api

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
 import api from "../../services/api";
-
 export default class Main extends Component {
+    componentDidMount() {
+        this.loadProducts();
+    }
+    
+    loadProducts =  async () => {
+        const response = await api.get('/products');
+
+        console.log(response);
+    };
     render(){
-        return <h1>Hello React</h1>;
+         return <h1>Hello React</h1>;
     }
 }
