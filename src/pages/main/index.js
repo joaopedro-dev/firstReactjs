26663b75021b38df5ab98ca195b06c48c6797b85
Micael Cid <micaelcid10@gmail.com>
2019-06-15 16:06:31 -0300

    Merging

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import api from "../../services/api";
 export default class Main extends Component {
     state = {
-        products :[]
+        products :[],
+        productName: '',
     };
 
     componentDidMount() {
@@ -11,19 +12,24 @@ export default class Main extends Component {
     
     loadProducts =  async () => {
         const response = await api.get('/products');
-
         this.setState({ products: response.data.docs });
-
     };
+
+    setProductName = (event) => {
+        this.setState({ productName: event.target.value });
+    };
+    
     render(){
         return(
             <div className="product-list">
+                <p>Adicionar tarefa</p>
+                <input type='text' onChange={this.setProductName} />
                 {this.state.products.map(product => (
-                        <article key={product._id}>
-                            <strong>{product.title}</strong>
-                            <p>{product.description}</p>
-                            <a href="">Acessar</a>
-                        </article>
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+                        <a href="">Acessar</a>
+                    </article>
                 ))}
             </div>
         );
