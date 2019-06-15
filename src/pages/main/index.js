6f3aaaa8f19e9658323009ae30afe61054e46520
Joao Pedro Barros <joaobarros11@hotmail.com>
2019-06-15 16:00:11 -0300

    Incluindo Descrição

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -19,7 +19,11 @@ export default class Main extends Component {
         return(
             <div className="product-list">
                 {this.state.products.map(product => (
-                    <h2>{product.title}</h2>
+                        <article key={product._id}>
+                            <strong>{product.title}</strong>
+                            <p>{product.description}</p>
+                            <a href="">Acessar</a>
+                        </article>
                 ))}
             </div>
         );
