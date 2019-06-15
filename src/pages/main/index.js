11899977ea33c585869692bbed35391d89df37fe
Joao Pedro Barros <joaobarros11@hotmail.com>
2019-06-15 16:46:57 -0300

    Merging with CID

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+
+import './styles.css';
 export default class Main extends Component {
     state = {
         products :[],
